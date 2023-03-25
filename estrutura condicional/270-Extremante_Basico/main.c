65d2c3a8e71f776e3dfa90dff2494c2d889aedc8
edgar vitor <vitoredgar888@gmail.com>
2023-03-25 16:28:39 -0300

    add 270 and 32

---
+++ estrutura condicional/270-Extremante_Basico/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int x,b,result;
+    scanf("%d %d", &x , &b);
+    result = x+b;
+    printf("X = %d", result);
+
+	return 0;
+}
---
+++ estrutura condicional/32-fahrenheit_para_Celsius/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float F,C;
+
+    scanf("%f",&F);
+    C = (F-32)/1.8;
+    printf("%.2f",C);
+
+	return 0;
+}
