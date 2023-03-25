9776b0e8cf02e9a5473d1ae7efede6f93ba895ae
edgar vitor <vitoredgar888@gmail.com>
2023-03-25 15:59:30 -0300

    add 8313 - Tabuada

---
+++ estrutura condicional/8312-Tabuada/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int number;
+    scanf("%d",&number);
+
+    printf("%d X 1 = %d\n", number, number);
+    printf("%d X 2 = %d\n", number , number * 2);
+    printf("%d X 3 = %d\n", number , number * 3);
+    printf("%d X 4 = %d\n", number , number * 4);
+    printf("%d X 5 = %d\n", number , number * 5);
+    printf("%d X 6 = %d\n", number , number * 6);
+    printf("%d X 7 = %d\n", number , number * 7);
+    printf("%d X 8 = %d\n", number, number * 8);
+    printf("%d X 9 = %d\n", number, number * 9);
+
+
+
+
+	return 0;
+}
